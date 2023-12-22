487eafda9f1b718d88f992761817d832a2f450f9
ChiragShukla222 <119029971+ChiragShukla222@users.noreply.github.com>
2023-12-22 10:11:14 +0530

    Add files via upload

---
+++ read.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+diabetes = pd.read_csv('diabetes.csv')
+print(diabetes.columns)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(diabetes.loc[:, diabetes.columns != 'Outcome'], diabetes['Outcome'], stratify=diabetes['Outcome'], random_state=66)
+training_accuracy = []
+test_accuracy = []
+# try n_neighbors from 1 to 10
+neighbors_settings = range(1, 11)
+for n_neighbors in neighbors_settings:
+    # build the model
+    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+    knn.fit(X_train, y_train)
+    # record training set accuracy
+    training_accuracy.append(knn.score(X_train, y_train))
+    # record test set accuracy
+    test_accuracy.append(knn.score(X_test, y_test))
+knn = KNeighborsClassifier(n_neighbors=9)
+knn.fit(X_train, y_train)
+print('Accuracy of K-NN classifier on training set: {:.2f}'.format(knn.score(X_train, y_train)))
+print('Accuracy of K-NN classifier on test set: {:.2f}'.format(knn.score(X_test, y_test)))
+predicted= knn.predict([[6,148,72,35,0,33.6,0.627,50]])
+print(predicted)
+
+
+
+logreg = LogisticRegression(C=100).fit(X_train, y_train)
+print("Training set score: {:.3f}".format(logreg.score(X_train, y_train)))
+print("Test set score: {:.3f}".format(logreg.score(X_test, y_test)))
+predicted= knn.predict([[1,89,66,23,94,28.1,0.167,21]])
+print(predicted)
+
+
